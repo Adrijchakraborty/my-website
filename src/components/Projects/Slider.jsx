879a59b9bc16878f74a228b9c26631d835ff55b7
Adrijchakraborty <adrijchakraborty8@gmail.com>
2024-10-24 20:20:53 +0530

    Project section updated

--- src/components/Projects/Slider.jsx
+++ src/components/Projects/Slider.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
+import { projects } from './projects.js';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -12,6 +13,8 @@ import 'swiper/css/pagination';
 
 // import required modules
 import { EffectCoverflow, Pagination } from 'swiper/modules';
+
+console.log(projects)
 
 
 export default function App() {
@@ -35,26 +38,27 @@ export default function App() {
             >
                 <SwiperSlide>
                     <div className='bg-white text-black flex flex-col gap-4 justify-center items-center h-full w-full rounded-lg'>
-                        <h1 className='uppercase text-3xl'>swipe right</h1>
+                        <h1 className='uppercase text-3xl font-roboto-slab'>swipe right</h1>
                         <FaArrowRight className='text-3xl' />
                     </div>
                 </SwiperSlide>
+                {projects.map((project, index) => {
+                    return (
+                        <SwiperSlide key={index}>
+                            <div className='bg-slate-200 h-full rounded-lg'>
+                                <img src={project.Image} className='min-h-[70%] border-4 border-black'/>
+                                <h1 className='text-center font-roboto-slab text-lg'>{project.Name}</h1>
+                                <div className='flex justify-around font-arima'>
+                                    <a href={project?.Live} target='_blank' className={`${project.Live &&"text-blue-600 hover:underline transition"}`} >Live</a>
+                                    <a href={project?.Github} target='_blank' className={`${project.Github &&"text-blue-600 hover:underline transition"}`}>Github</a>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    )
+                })}
                 <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-                    <button onClick={() => console.log("Clicked2")}>Hi</button>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className='bg-white text-black flex flex-col gap-4 justify-center items-center h-full w-full rounded-lg'>
-                        <h1 className='uppercase text-3xl'>swipe left</h1>
+                    <div className='bg-white text-black flex flex-col gap-4 justify-center items-center h-full w-full rounded-lg border-2 border-black'>
+                        <h1 className='uppercase text-3xl font-roboto-slab'>swipe left</h1>
                         <FaArrowLeft className='text-3xl' />
                     </div>
                 </SwiperSlide>
---
+++ src/components/Projects/projects.js
@@ -0,0 +1,33 @@
+import Project1 from "../../assets/Project-1.jpg"
+import Project2 from "../../assets/Project-2.jpg"
+import Project3 from "../../assets/Project-3.jpg"
+import Project4 from "../../assets/Project-4.jpg"
+
+const projects = [
+    {
+        "Image": Project1,
+        "Name": "Colab&Work",
+        "Live":null,
+        "Github":"https://github.com/Adrijchakraborty/Colab-and-Work"
+    },
+    {
+        "Image": Project2,
+        "Name": "Estate App",
+        "Live":null,
+        "Github":"https://github.com/Adrijchakraborty/mern"
+    },
+    {
+        "Image": Project3,
+        "Name": "ChatApp",
+        "Live":"https://chatapp-7mfe.onrender.com/",
+        "Github":"https://github.com/Adrijchakraborty/chatapp"
+    },
+    {
+        "Image": Project4,
+        "Name": "Ecommerce",
+        "Live":"https://euphonious-donut-4e8bbe.netlify.app/",
+        "Github":null
+    },
+]
+
+export {projects}
